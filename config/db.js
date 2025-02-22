4c2df217335e1fa11dddeea70379f1f5a4dcbbbc
shafeeq <shafeeq@onlinecheckwriter.com>
2025-02-22 16:05:49 +0530

    updated

--- config/db.js
+++ config/db.js
@@ -4,7 +4,8 @@ const pool = mysql.createPool({
     host: '127.0.0.1',
     user: 'root',    // Replace with your MySQL username
     password: '', // Replace with your MySQL password
-    database: 'node'  // Replace with your database name
+    database: 'node',  // Replace with your database name,
+    port: 3307
 });
 
 module.exports = pool.promise();
